affaedf2061e791ac58e70840a35038c4e370883
Bello <dgbello.dev@hotmail.com>
2024-04-15 21:54:51 -0300

    first commit

---
+++ backend/src/main/java/br/com/douglasbello/cinelist/CinelistApplication.java
@@ -0,0 +1,12 @@
+package br.com.douglasbello.cinelist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinelistApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CinelistApplication.class, args);
+    }
+}
---
+++ backend/src/test/java/br/com/douglasbello/cinelist/CinelistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.douglasbello.cinelist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinelistApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
