4188ffa7fec6386da560b0926c29f29b8762f2d0
Douglas Bello <dgbello.dev@hotmail.com>
2024-04-22 17:23:35 -0300

    updates

--- backend/src/main/java/br/com/douglasbello/cinelist/services/impl/UserService.java
+++ backend/src/main/java/br/com/douglasbello/cinelist/services/impl/UserService.java
@@ -3,11 +3,14 @@ package br.com.douglasbello.cinelist.services.impl;
 import br.com.douglasbello.cinelist.entities.User;
 import br.com.douglasbello.cinelist.repositories.UserRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService implements br.com.douglasbello.cinelist.services.Service<User> {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
@@ -16,5 +19,12 @@ public class UserService implements br.com.douglasbello.cinelist.services.Servic
     @Override
     public JpaRepository<User, String> getRepository() {
         return userRepository;
+    }
+
+    @Override
+    public User save(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+
+        return userRepository.save(user);
     }
 }
